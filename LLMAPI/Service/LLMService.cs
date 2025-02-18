f51afb450c43340e71cbf1b2bae0bef243b4c440
Severin <sevjoh23@gmail.com>
2025-02-18 15:26:05 +0100

    adding descriptive alt texts 1/x

--- LLMAPI/Service/LLMService.cs
+++ LLMAPI/Service/LLMService.cs
@@ -118,5 +118,59 @@ namespace LLMAPI.Service
                 return $"Error processing image: {ex.Message}";
             }
         }
+
+        public async Task<string> GenerateContent(
+            string projectId = "rich-world-450914-e6",
+            string location = "europe-west4",
+            string publisher = "google",
+            string model = "gemini-1.5-flash-001"
+            )
+        {
+            // Create client
+            var predictionServiceClient = new PredictionServiceClientBuilder
+            {
+                Endpoint = $"{location}-aiplatform.googleapis.com"
+            }.Build();
+
+            // Initialize content request
+            var generateContentRequest = new GenerateContentRequest
+            {
+                Model = $"projects/{projectId}/locations/{location}/publishers/{publisher}/models/{model}",
+                GenerationConfig = new GenerationConfig
+                {
+                    Temperature = 0.4f,
+                    TopP = 1,
+                    TopK = 32,
+                    MaxOutputTokens = 2048
+                },
+                Contents =
+                {
+                    new Content
+                    {   
+                        Role = "USER",
+                        Parts =
+                        {
+                            new Part { Text = "What's in this photo?" },
+                            new Part { FileData = new() { MimeType = "image/png", FileUri = "gs://generativeai-downloads/images/scones.jpg" } }
+                        }
+                    }   
+                }
+            };
+
+            // Make the request, returning a streaming response
+            using PredictionServiceClient.StreamGenerateContentStream response = predictionServiceClient.StreamGenerateContent(generateContentRequest);
+
+            StringBuilder fullText = new();
+
+            // Read streaming responses from server until complete
+            AsyncResponseStream<GenerateContentResponse> responseStream = response.GetResponseStream();
+            await foreach (GenerateContentResponse responseItem in responseStream)
+            {
+                fullText.Append(responseItem.Candidates[0].Content.Parts[0].Text);
+            }
+
+            return fullText.ToString();
+        }
     }
+
 }
